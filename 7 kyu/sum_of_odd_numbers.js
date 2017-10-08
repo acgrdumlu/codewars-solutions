6aac9540374502292871c242cb0ca7a3875f78ac
acgrdumlu <cgrdumlu@gmail.com>
2017-10-08 16:38:42 +0300

    Sum of odd numbers solution added

---
+++ 7 kyu/sum_of_odd_numbers.js
@@ -0,0 +1,13 @@
+//              1
+//           3     5
+//        7     9    11
+//    13    15    17    19
+// 21    23    25    27    29
+
+
+function rowSumOddNumbers(n) {
+  return Math.pow(n, 3);
+}
+
+//Other Solution
+const rowSumOddNumbers = n => Math.pow(n, 3);
