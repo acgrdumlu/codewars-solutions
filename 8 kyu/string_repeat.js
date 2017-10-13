b366814c958c3d47bd2610532cd15dc5e8cf389c
acgrdumlu <cgrdumlu@gmail.com>
2017-10-13 08:07:33 +0300

    String Repeat solution added

---
+++ 8 kyu/string_repeat.js
@@ -0,0 +1,14 @@
+//First Solution
+
+function repeatStr(n, s) {
+    var nStr = '';
+    for (var i = 0; i < n; i++) {
+        nStr = nStr + s;
+    };
+    return news;
+}
+
+
+//Best Solution 
+
+repeatStr = (n, s) => s.repeat(n)
