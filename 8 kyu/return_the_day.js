d9f6b8017ec359f1dc9a2c527d2a3a700e88f220
acgrdumlu <cgrdumlu@gmail.com>
2017-10-17 19:17:28 +0300

    Return the Day solution added

---
+++ 8 kyu/return_the_day.js
@@ -0,0 +1,24 @@
+function whatday(num) { 
+  switch(num) {
+    case 1:
+      return "Sunday";
+    case 2:
+      return "Monday";
+    case 3:
+      return "Tuesday";
+    case 4:
+      return "Wednesday";
+    case 5:
+      return "Thursday";
+    case 6:
+      return "Friday";
+    case 7:
+      return "Saturday";
+    default:
+      return 'Wrong, please enter a number between 1 and 7';
+  }
+}
+
+//Best Solution
+
+whatday=n=>[,'Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'][n]||'Wrong, please enter a number between 1 and 7'
